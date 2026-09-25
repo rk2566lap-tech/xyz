6a72a543a4337aa30e2d88fdcdddd9dd6f60315e
rk2566lap-tech <rk2566lap@gmail.com>
2026-09-25 13:15:20 +0530

    Add files via upload

---
+++ links.php
@@ -0,0 +1,11 @@
+<?php
+  $links = array(
+    'css' => 'lib/lightbox/css/lightbox.min.css',
+    'js' => 'lib/lightbox/js/lightbox.min.js'
+  );
+?>
+<?php
+  $links = array(
+    'js' => 'lib/waypoints/waypoints.min.js'
+  );
+?>
